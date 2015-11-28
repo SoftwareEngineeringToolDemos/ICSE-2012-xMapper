4d3e3ca66d66be09e439896107d153dc8f4202d5
varunaettapu <varun.aettapu@gmail.com>
2015-11-28 08:35:45 -0500

    Update screenlock_disable.sh

--- build-vm/vm-contents/screenlock_disable.sh
+++ build-vm/vm-contents/screenlock_disable.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 /usr/bin/gsettings set org.gnome.desktop.lockdown disable-lock-screen true
-echo $?
+/usr/bin/gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 /usr/bin/gsettings set org.gnome.desktop.screensaver lock-enabled false
-echo $?
